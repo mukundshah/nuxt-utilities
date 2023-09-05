321815b71479cc7ce4c37d8813a510576c92f66e
Mukund Shah <39938037+mukundshah@users.noreply.github.com>
2023-09-05 18:34:37 +0545

    feat: add `useAPIView`

--- packages/viewset/src/useAPIView.ts
+++ packages/viewset/src/useAPIView.ts
@@ -1,140 +1,234 @@
-import { createInsertSchema, createSelectSchema } from "drizzle-zod";
-import { getTableConfig } from "drizzle-orm/pg-core";
+import { z } from 'zod';
+import { getQuery, getRouterParam, readBody } from 'h3';
+import { eq, getTableColumns, sql } from 'drizzle-orm';
+import { createInsertSchema, createSelectSchema } from 'drizzle-zod';
 
-import type { H3Event } from "h3";
-import type { PostgresJsDatabase } from "drizzle-orm/postgres-js";
-import type { PgTableWithColumns } from "drizzle-orm/pg-core";
-import type { ZodBigInt, ZodNumber, ZodObject, ZodString } from "zod";
+import type { H3Event } from 'h3';
+import type { ZodObject } from 'zod';
+import type { Column, SQL } from 'drizzle-orm';
+import type { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
+import type { PgColumn, PgTableWithColumns } from 'drizzle-orm/pg-core';
 
 type Database = PostgresJsDatabase;
 
-type ZodPrimaryKey = ZodBigInt | ZodNumber | ZodString;
+type ColumnDescriptor<Table extends PgTableWithColumns<any>> = keyof Table['_']['columns'];
 
-interface Model extends PgTableWithColumns<any> {}
-
-interface APIViewOptions<T extends PgTableWithColumns<any>> {
-  model: T;
-  db: Database;
-  request: H3Event;
+interface APIViewOptions<
+  Table extends PgTableWithColumns<any>,
+  ZDefault extends ZodObject<any>,
+  ZList extends ZodObject<any> = ZDefault,
+  ZCreate extends ZodObject<any> = ZDefault,
+  ZRetrieve extends ZodObject<any> = ZDefault,
+  ZUpdate extends ZodObject<any> = ZDefault,
+> {
+  model: Table
+  db: Database
+  request: H3Event
+  primaryKey?: ColumnDescriptor<Table> | {
+    retrieve?: ColumnDescriptor<Table>
+    update?: ColumnDescriptor<Table>
+    delete?: ColumnDescriptor<Table>
+    default: ColumnDescriptor<Table>
+  }
   schema?: {
-    identifier?: keyof T["_"]["columns"];
-    default?: ZodObject<any>;
-    list?: ZodObject<any>;
-    create?: ZodObject<any>;
-    update?: {
-      pk: ZodPrimaryKey;
-      values: ZodObject<any>;
-    };
-    retrieve?: ZodPrimaryKey;
-    destroy?: ZodPrimaryKey;
-  };
-  permissions?: Record<string, any>;
-  filters: Record<string, any>;
-  ordering?: Record<string, "asc" | "desc">;
+    list?: ZList
+    create?: ZCreate
+    update?: ZUpdate
+    retrieve?: ZRetrieve
+  }
+  pageSize?: number
+  searchFields?: Array<ColumnDescriptor<Table>>
+  filterFields?: Array<ColumnDescriptor<Table>>
+  orderFields?: Array<ColumnDescriptor<Table>>
 }
 
-type LogicalOperator = "$and" | "$or" | "$not";
-type ComparisonOperator =
-  | "$eq"
-  | "$neq"
-  | "$gt"
-  | "$gte"
-  | "$lt"
-  | "$lte"
-  | "$is"
-  | "$isNull"
-  | "$in";
+function getOrderBySQL<Table extends PgTableWithColumns<any>>(table: Table, ordering: Record<ColumnDescriptor<Table>, 'asc' | 'desc'>) {
+  const sqlChunks: SQL[] = [];
+  Object.keys(ordering).forEach((columnName) => {
+    const column = table[columnName as ColumnDescriptor<Table>];
+    const direction = ordering[columnName as ColumnDescriptor<Table>];
+    sqlChunks.push(sql`${column} ${direction}`);
+  });
 
-type Filter = {
-  // Omit<[key in LogicalOperaor], '$not'>?: Filter[]
-  [key in LogicalOperator]?: Filter[];
-} & {
-  [key in ComparisonOperator]?: any;
-};
-
-function useFilter(filters: Record<string, any>) {}
-
-function useOrdering(orderBy: Record<string, "asc" | "desc">) {}
-
-function useFileUpload(provider: "aws" | "gcp" | "azure") {
-  function upload() {}
-
-  return {
-    upload,
-  };
+  return sql.join(sqlChunks, sql`, `);
 }
 
-const useAPIView = <T extends PgTableWithColumns<any>>(
-  options: APIViewOptions<T>
-) => {
+export function fieldsToColumns<
+T extends PgTableWithColumns<any>,
+Z extends ZodObject<any>,
+>(model: T, schema: Z) {
+  const attrs = Object.keys(schema.shape as Record<string, any>);
+
+  const fields = attrs.reduce((acc, attr) => {
+    acc[attr] = model[attr] as ReturnType<typeof model['attr']>;
+    return acc;
+  }, {} as Record<string, PgColumn>);
+
+  return fields;
+}
+
+function getTablePrimaryKeys<T extends PgTableWithColumns<any>>(table: T) {
+  const columns = getTableColumns(table) as Record<string, Column>;
+  return Object.keys(columns).filter(columnName => columns[columnName].primary);
+}
+
+export function useAPIView<
+Table extends PgTableWithColumns<any>,
+ZDefault extends ZodObject<any>,
+ZList extends ZodObject<any>,
+ZCreate extends ZodObject<any>,
+ZRetrieve extends ZodObject<any>,
+ZUpdate extends ZodObject<any> ,
+>(options: APIViewOptions<Table, ZDefault, ZList, ZCreate, ZRetrieve, ZUpdate>) {
   const {
     model,
     db,
     request,
     schema,
-    filters: _filters,
-    ordering,
-    permissions,
+    primaryKey,
+    pageSize,
+    searchFields,
+    filterFields,
+    orderFields,
   } = options;
 
-  // pk of model
-  const { primaryKeys: pk } = getTableConfig(model);
+  const pk = getTablePrimaryKeys(model)[0] as ColumnDescriptor<Table>;
 
-  // TODO: If pk is not defined, check if alternate field is provided else throw error that a pk or field is required
+  const _primaryKey: Record<string, ColumnDescriptor<Table>> = {
+    retrieve: typeof primaryKey === 'object' ? primaryKey.retrieve ?? primaryKey.default : primaryKey ?? pk,
+    update: typeof primaryKey === 'object' ? primaryKey.update ?? primaryKey.default : primaryKey ?? pk,
+    delete: typeof primaryKey === 'object' ? primaryKey.delete ?? primaryKey.default : primaryKey ?? pk,
+  };
 
-  const defaultSchema = schema?.default ?? createSelectSchema(model);
-  const listSchema = schema?.list ?? defaultSchema ?? createSelectSchema(model);
-  const createSchema = schema?.create ?? createInsertSchema(model);
-  const retrieveSchema = schema?.retrieve ?? createSelectSchema(model);
-  const updateSchema =
-    schema?.update ??
-    ({
-      pk: pk[0],
-      values: defaultSchema ?? createInsertSchema(model),
-    } as const);
-  const destroySchema = schema?.destroy ?? pk[0];
+  {
+    const undefinedKeys = Object.keys(_primaryKey).filter(key => _primaryKey[key] === undefined);
+    if (undefinedKeys.length > 0)
+      throw new Error(`primaryKey[${undefinedKeys.join('|')}] is undefined`);
+  }
+
+  const _schema = {
+    list: schema?.list ?? createSelectSchema(model),
+    retrieve: schema?.retrieve ?? createSelectSchema(model),
+    create: schema?.create ?? createInsertSchema(model),
+    update: schema?.update ?? createInsertSchema(model),
+  };
+
+  const _pageSize = pageSize ?? 25;
 
   // TODO: parse filters and ordering from request.query with zod
 
   async function list(
-    schema: ZodObject<any> = listSchema,
-    filters: Record<string, any> = _filters
+    schema: ZList = _schema.list as ZList,
+
   ) {
-    await db.select(schema.parse({})).from(model).where(filters);
-    return [];
+    const results = await db.select(fieldsToColumns(model, schema)).from(model);
+    return z.array(schema).parse(results);
   }
 
-  async function create(schema: ZodObject<any> = createSchema) {
-    await db.insert(model).values(createSchema.parse({})).returning("*");
-    return [];
+  async function paginatedList(
+    schema: ZList = _schema.list as ZList,
+    pageSize: number = _pageSize,
+  ) {
+    const query = getQuery(request);
+    const page = query.page as number ?? 1;
+
+    let dbQuery = db.select({
+      ...fieldsToColumns(model, schema),
+      count: sql<number>`count(*) over()`.mapWith(Number),
+    }).from(model);
+
+    if (query.filters && filterFields) {
+      // TODO: parse filters with zod
+    }
+
+    if (query.order && orderFields) {
+      // TODO: parse order with zod
+      const orderingSchema = z.record(
+        z.string().refine(value => orderFields?.includes(value)),
+        z.enum(['asc', 'desc']),
+      );
+      const ordering = orderingSchema.parse(query.ordering ?? {}) as Record<ColumnDescriptor<Table>, 'asc' | 'desc'>;
+      const orderBySQL = getOrderBySQL(model, ordering);
+      if (orderBySQL)
+        dbQuery = dbQuery.orderBy(getOrderBySQL(model, ordering));
+    }
+
+    const results = await dbQuery.limit(pageSize).offset((page - 1) * pageSize);
+
+    const count = results[0].count;
+
+    return {
+      page,
+      pageSize,
+      count,
+      pageCount: Math.ceil(count / pageSize),
+      results: z.array(schema).parse(results),
+    };
   }
 
-  async function retrieve(identifier: ZodPrimaryKey = retrieveSchema) {
-    await db.select(retrieveSchema.parse({})).from(model).where(filters);
-    return [];
+  async function create(schema: ZCreate = _schema.create as ZCreate) {
+    const body = await readBody<z.infer<ZCreate>>(request);
+    const values = schema.parse(body) as z.infer<ZCreate>;
+    const result = await db.insert(model).values(values).returning();
+    return schema.parse(result[0]) as z.infer<ZCreate>;
+  }
+
+  async function retrieve(
+    primaryKey: ColumnDescriptor<Table> = _primaryKey.retrieve,
+    schema: ZRetrieve = _schema.retrieve as ZRetrieve,
+  ) {
+    const pk = getRouterParam(request, primaryKey.toString());
+    const results = await db.select(fieldsToColumns(model, schema)).from(model).where(eq(model[primaryKey], pk));
+    return schema.parse(results[0]) as z.infer<ZRetrieve>;
   }
 
   async function update(
-    identifier: ZodPrimaryKey = updateSchema.pk,
-    schema: ZodObject<any> = updateSchema.values
+    primaryKey: ColumnDescriptor<Table> = _primaryKey.update,
+    schema: ZUpdate = _schema.update as ZUpdate,
   ) {
-    await db.update(model).set(updateSchema.parse({})).where(filters);
-    return [];
+    const pk = getRouterParam(request, primaryKey.toString());
+    const body = await readBody<z.infer<ZCreate>>(request);
+    const values = schema.parse(body) as z.infer<ZCreate>;
+    const result = await db.update(model).set(values).where(eq(model[primaryKey], pk)).returning();
+    return _schema.retrieve.parse(result[0]) as z.infer<ZRetrieve>;
   }
 
-  async function destroy(identifier: ZodPrimaryKey = destroySchema) {
-    return [];
+  async function destroy(primaryKey: ColumnDescriptor<Table> = _primaryKey.delete) {
+    const pk = getRouterParam(request, primaryKey.toString());
+    const deletedIds = await db.delete(model).where(eq(model[primaryKey], pk)).returning({ deletedId: model[primaryKey] });
+    return deletedIds[0];
   }
 
   async function search() {
-    return [];
+    // TODO: implement search
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve([]);
+      }, 1000);
+    },
+    );
+  }
+
+  async function all() {
+    // how to handle detail and list in one view?
+    if (request.method === 'GET')
+      return await list();
+    if (request.method === 'POST')
+      return create();
+    if (request.method === 'PUT')
+      return update();
+    if (request.method === 'DELETE')
+      return destroy();
   }
 
   return {
     list,
+    paginatedList,
     create,
     retrieve,
     update,
     destroy,
+    search,
+    all,
   };
-};
+}
